1155bcbe72e6a3b1ccc7d212a23696420969a520
kylezou <31173219+morningkyle@users.noreply.github.com>
2017-11-19 07:18:12 +0800

    (1)code refactoring.

--- examples/display_mnist_image.py
+++ examples/display_mnist_image.py
@@ -1,26 +1,35 @@
+import sys
 import matplotlib.pyplot as plt
 from mnist import MNIST
 
 
-def load_data():
-    mndata = MNIST('../mnist/data', return_type='numpy')
+def load_data(data_path):
+    mndata = MNIST(data_path, return_type='numpy')
     images, labels = mndata.load_training()
     return images, labels
 
 
-def display_data_info(images, labels):
-    print("type of images container: {0}, type of element: {1}".format(type(images), type(images[0])))
-    print("number of images: {0}".format(len(images)))
-    print("type of labels container: {0}, type of element: {1}".format(type(labels), type(labels[0])))
-    print("number of labels: {0}".format(len(labels)))
+def show_image(image, label):
+    plt.title(label)
+    plt.imshow(image.reshape(28, 28))
+    plt.gray()
+    plt.show()
+
+
+def print_data_info(images, labels):
+    print("Type of images container: {0}, type of element: {1}".format(type(images), type(images[0])))
+    print("Type of labels container: {0}, type of element: {1}".format(type(labels), type(labels[0])))
+    print("Total images: {0}, Total labels: {0}".format(len(images), len(labels)))
 
 
 if __name__ == '__main__':
-    images, labels = load_data()
-    display_data_info(images, labels)
-    plt.title(labels[10])
-    plt.imshow(images[10].reshape(28, 28))
-    plt.gray()
-    plt.show()
+    if len(sys.argv) > 1:
+        idx = int(sys.argv[1])
+    else:
+        idx = 0
+    images, labels = load_data('../mnist/data')
+    print_data_info(images, labels)
+    show_image(images[idx], labels[idx])
+
 
 
