83d88990ff95a8213ff40147dea1ce0df5996205
kylezou <31173219+morningkyle@users.noreply.github.com>
2017-11-19 17:53:58 +0800

    fix running ann test

--- examples/ann_mnist_test.py
+++ examples/ann_mnist_test.py
@@ -16,31 +16,13 @@ mndata = MNISTLoader('../dlimage//mnist/data')
 images, labels = mndata.load_training()
 training_x = [i.reshape(784, 1) for i in images.copy()]
 training_y = [vectorized_result(i) for i in labels.copy()]
-print(len(training_x), len(training_x))
-print(type(training_x[0]))
-print(training_x[0].shape)
-print(training_x[0].dtype)
-
-print(type(training_y[0]))
-print(training_y[0].dtype)
-print(training_y[0].shape)
-print('------------------')
 training_data = zip(training_x, training_y)
 
 images, labels = mndata.load_testing()
 test_x = [i.reshape(784, 1) for i in images]
-test_y = [vectorized_result(i) for i in labels]
-print(len(test_x), len(test_y))
-print(type(test_x[0]))
-print(test_x[0].shape)
-print(test_x[0].dtype)
-
-print(type(test_y[0]))
-print(test_y[0].dtype)
-print(test_y[0].shape)
-test_data = zip(test_x, test_y.copy())
+test_data = zip(test_x, labels)
 
 
-net = Network([784, 10, 10])
-net.SGD(training_data, 10, 10, 1.0, test_data=test_data)
+net = Network([784, 30, 10])
+net.SGD(training_data, 100, 200, 0.98, test_data=test_data)
 
